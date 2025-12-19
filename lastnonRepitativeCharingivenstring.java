a1abcaa58f7d7678de9d4b757398a516463b17d6
Gowri Prabhu Kiran <152252094+GowriPrabhu5995@users.noreply.github.com>
2025-12-19 04:27:59 +0000

    Refactor lastnonRepitativeCharingivenstring class to improve variable naming for clarity

--- lastnonRepitativeCharingivenstring.java
+++ lastnonRepitativeCharingivenstring.java
@@ -1,6 +1,7 @@
 public class lastnonRepitativeCharingivenstring {
     public static void main(String[] args) {
-        String str = "temperature";
+        String original = "temperature";
+        String str = original.toLowerCase();
         char result = '\0';
         for (int i = str.length() - 1; i >= 0; i--) {
             int count = 0;
