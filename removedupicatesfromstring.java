29440b56516558f31729ee723852dab1cba17650
Gowri Prabhu Kiran <152252094+GowriPrabhu5995@users.noreply.github.com>
2025-12-20 06:15:24 +0000

    removedupicatesfromstring

---
+++ removedupicatesfromstring.java
@@ -0,0 +1,22 @@
+public class removedupicatesfromstring{
+
+    public static void main(String[] args){
+        String str = "banana";
+        String result = "";
+        for(int i=0; i<str.length(); i++){
+            int count = 0;
+            if(str.charAt(i)!= ' '){
+                for(int j=i+1; j<str.length();j++){
+                    if(str.charAt(i)== str.charAt(j) && i!=j){
+                        count++;
+                    }
+                }
+                if(count ==0){
+                    result+=str.charAt(i);
+                }
+            }
+        }
+        System.out.println("the Elements after removing duplicate elements are: " + result);
+    }
+
+}
