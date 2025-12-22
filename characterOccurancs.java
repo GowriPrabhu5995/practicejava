9e76c103ccc9fbe12a5f88af0f64ea2f1f2b5678
Gowri Prabhu Kiran <152252094+GowriPrabhu5995@users.noreply.github.com>
2025-12-22 03:53:09 +0000

    character occurance

---
+++ characterOccurancs.java
@@ -0,0 +1,24 @@
+public class characterOccurancs {
+    public static void main(String[] args){
+        String original = "automation";
+        String str=original.toLowerCase();
+
+        for(int i=0; i<str.length();i++){
+            int count=1;
+            if(str.charAt(i)=='0')
+                continue;
+            
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                    // Mark as counted
+                    str = str.substring(0, j) + '0' + str.substring(j + 1);
+                }
+            }
+
+            System.out.println(str.charAt(i) + " = " + count);
+
+        }
+    }
+    }
+
